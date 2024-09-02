3c702ff0ca4f508224bc6dbe159c8194dec082bb
stefanusadriirawan <stefanusadriirawan@gmail.com>
2024-09-02 07:03:22 +0700

    Dummy API

---
+++ systemCore/adelaideEngineMain/DummyZephy/CMakeFiles/my_crow_app.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/my_crow_app.dir/dummy.cpp.o"
+  "CMakeFiles/my_crow_app.dir/dummy.cpp.o.d"
+  "my_crow_app"
+  "my_crow_app.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/my_crow_app.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ systemCore/adelaideEngineMain/DummyZephy/CMakeFiles/my_crow_app.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for my_crow_app.
+# This may be replaced when dependencies are built.
