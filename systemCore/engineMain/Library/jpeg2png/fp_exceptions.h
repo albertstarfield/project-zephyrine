4152488ab9460ff5b8b285d397480dd692614552
Albert Starfield Wahyu Suryo Sam <albertstarfield2001@gmail.com>
2025-02-04 10:12:29 +0700

    Goodbye C++

--- systemCore/engineMain/Library/jpeg2png/fp_exceptions.h
+++
@@ -1,6 +0,0 @@
-#ifndef JPEG2PNG_FP_EXCEPTIONS_H
-#define JPEG2PNG_FP_EXCEPTIONS_H
-
-void enable_fp_exceptions(void);
-
-#endif
--- systemCore/engineMain/compile_command_test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-rm -rf build
-cmake -B build -S . && cmake --build build
-./build/main
