524debf8d7cd2d0a5b7e7babafa368712738040d
Albert Starfield Wahyu Suryo Samudro <albertstarfield2001@gmail.com>
2026-05-22 01:43:07 +0700

    automatic_porting_checkpoint_Fri May 22 01:43:07 WIB 2026

--- Adelaide_Lite/src/adelaide_server_pkg.ads
+++ Adelaide_Lite/src/adelaide_server_pkg.ads
@@ -1,10 +1,16 @@
 with AWS.Response;
 with AWS.Status;
+with Streaming_Queue;
 
 package Adelaide_Server_Pkg is
    pragma Spark_Mode (Off);
 
    --  The main dispatch callback for Adelaide AWS Proxy Server
    function Dispatch (Request : AWS.Status.Data) return AWS.Response.Data;
+
+   --  Session Management for cross-component logging
+   procedure Register (ID : String; Q : Streaming_Queue.Queue_Access);
+   procedure Unregister (ID : String);
+   procedure Push_Log (ID : String; Log : String);
 
 end Adelaide_Server_Pkg;
