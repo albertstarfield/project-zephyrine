ecf9637bacf998c803c69b40505c2a879788439b
Adelaide Proxy <adelaide@proxy.local>
2026-05-22 18:59:08 +0700

    automatic_porting_checkpoint_Fri May 22 18:59:08 WIB 2026

--- Adelaide_Lite/src/database_manager.ads
+++ Adelaide_Lite/src/database_manager.ads
@@ -12,7 +12,7 @@ package Database_Manager is
    procedure Add_To_Cache (Prompt : String; Embedding : Math_Utils.Vector; Response : String);
 
    --  Semantic Retrieval from Cache
-   function Get_Cached_Response (Embedding : Math_Utils.Vector) return String;
+   function Get_Cached_Response (Embedding : Math_Utils.Vector; WCET : Duration) return String;
 
    --  Simple keyword recall (Existing logic)
    function Recall (Query : String) return String;
