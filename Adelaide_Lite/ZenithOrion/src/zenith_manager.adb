86c6a4cfcab08f4cc3734ce39f7ef6673bddda44
Albert Starfield Wahyu Suryo Samudro <albertstarfield2001@gmail.com>
2026-05-24 03:11:01 +0700

    automatic_porting_checkpoint_Sun May 24 03:11:01 WIB 2026

--- Adelaide_Lite/ZenithOrion/src/zenith_manager.adb
+++ Adelaide_Lite/ZenithOrion/src/zenith_manager.adb
@@ -1,10 +1,13 @@
 with Ada.Calendar;
 with Ada.Real_Time;
+with Zenith_Orion;
 
 package body Zenith_Manager with SPARK_Mode => On is
 
    protected body Telemetry_Store is
-      procedure Update (Timing : Duration; Jitter_Max : Duration; Jitter_Avg : Duration) is
+      procedure Update
+        (Timing : Duration; Jitter_Max : Duration; Jitter_Avg : Duration)
+      is
       begin
          Current_Timing := Timing;
          Current_J_Max  := Jitter_Max;
@@ -17,23 +20,19 @@ package body Zenith_Manager with SPARK_Mode => On is
    end Telemetry_Store;
 
    task body Zenith_Orion_Task is
-      use type Ada.Calendar.Time;
-      Last_Jitter_Reset : Ada.Calendar.Time := Ada.Calendar.Clock;
    begin
       accept Start;
       Zenith_Orion.Initialize;
       loop
          Zenith_Orion.Paced_Loop;
          
-         --  Update local telemetry store (SPARK-friendly)
          declare
-            J : constant Zenith_Orion.Jitter_Data := Zenith_Orion.Get_Jitter_Profile;
+            J : constant Zenith_Orion.Jitter_Data :=
+              Zenith_Orion.Get_Jitter_Profile;
          begin
-            Telemetry_Store.Update (Zenith_Orion.Get_Current_Timing, J.Max_Jitter, J.Avg_Jitter);
+            Telemetry_Store.Update
+              (Zenith_Orion.Get_Current_Timing, J.Max_Jitter, J.Avg_Jitter);
          end;
-         
-         --  The interval for jitter reset logic can be managed here if needed
-         --  but we'll just update every loop for simplicity in the manager.
       end loop;
    end Zenith_Orion_Task;
 
