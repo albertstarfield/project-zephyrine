9bc9f7273be61cda35717550328ffed7ee4e56c4
Albert Starfield Wahyu Suryo Samudro <albertstarfield2001@gmail.com>
2026-05-24 20:51:43 +0700

    automatic_porting_checkpoint_Sun May 24 20:51:43 WIB 2026

--- Adelaide_Lite/src/kokoro_interface.ads
+++ Adelaide_Lite/src/kokoro_interface.ads
@@ -2,7 +2,7 @@ with Ada.Streams;
 
 package Kokoro_Interface is
 
-   --  Synthesizes speech using the Kokoro Python sidecar on port 11421.
+   --  Synthesizes speech using the Kokoro Python CLI tool.
    --  Returns the raw WAV file bytes.
    function Synthesize_Speech (Text : String) return Ada.Streams.Stream_Element_Array;
 
