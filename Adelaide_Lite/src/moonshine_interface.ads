30b03b2801536a5fd56049df4394f9c395784728
Adelaide Proxy <adelaide@proxy.local>
2026-05-24 13:45:08 +0700

    automatic_porting_checkpoint_Sun May 24 13:45:08 WIB 2026

--- Adelaide_Lite/src/moonshine_interface.ads
+++ Adelaide_Lite/src/moonshine_interface.ads
@@ -7,6 +7,6 @@ package Moonshine_Interface is
    procedure Init_Moonshine (Model_Path : String);
    
    --  Transcribe expects raw 16KHz floats
-   function Transcribe_Raw_PCM (Audio_Data : access float; Audio_Length : Interfaces.Unsigned_64) return String;
+   function Transcribe_Raw_PCM (Audio_Data : access Float; Audio_Length : Interfaces.Unsigned_64) return String;
    
 end Moonshine_Interface;
