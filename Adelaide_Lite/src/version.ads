f701849ecef55e14faaea97b3377b23fe129d8b2
Albert Starfield Wahyu Suryo Samudro <albertstarfield2001@gmail.com>
2026-06-26 22:23:18 +0700

    Fix compilation issues and enhance Tensor Accelerator monitoring

--- Adelaide_Lite/src/version.ads
+++ Adelaide_Lite/src/version.ads
@@ -4,6 +4,6 @@
 --  ============================================================================
 package Version is
    Project_Name : constant String := "Project Zephyrine Adelaide Volatus Damare AWS";
-   Git_Hash     : constant String := "214dd9d";
+   Git_Hash     : constant String := "4f6a3ae";
    Full_Version : constant String := Project_Name & " - " & Git_Hash;
 end Version;
