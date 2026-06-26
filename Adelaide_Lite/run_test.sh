cf64ce3ace2faed0a844a9a47bf70f0dd2dbe268
Adelaide Proxy <adelaide@proxy.local>
2026-06-26 22:37:24 +0700

    Checkpoint: TestSuite.sh restored, ready for overnight fixes

--- Adelaide_Lite/run_test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd /Users/albertstarfield/LibraryTube/OpenIntellegentiaPlatform/Adelaide_Lite
-alr exec -- ./bin/adelaide_server &
-SERVER_PID=$!
-sleep 5
-curl -X POST http://127.0.0.1:11420/api/chat -d '{"model":"zephy","messages":[{"role":"user","content":"What is anarchy?"}]}'
-kill $SERVER_PID
