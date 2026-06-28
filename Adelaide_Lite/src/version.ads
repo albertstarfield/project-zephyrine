e2eddd9e41741e414378faf1fc4b2cd61d5a56a0
Adelaide Proxy <adelaide@proxy.local>
2026-06-28 07:44:58 +0700

    Implement Speculative Decoding loops and INOP CPU Fallback in Generate

--- Adelaide_Lite/src/version.ads
+++ Adelaide_Lite/src/version.ads
@@ -4,6 +4,6 @@
 --  ============================================================================
 package Version is
    Project_Name : constant String := "Project Zephyrine Adelaide Volatus Damare AWS";
-   Git_Hash     : constant String := "38b0bcd";
+   Git_Hash     : constant String := "cc2abac";
    Full_Version : constant String := Project_Name & " - " & Git_Hash;
 end Version;
