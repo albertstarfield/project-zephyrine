b216ada0afcc3292da879641f5410e81afe9abf4
Albert Starfield Wahyu Suryo Samudro <albertstarfield2001@gmail.com>
2026-06-28 12:26:43 +0700

    Add fcntl.flock to run.py to enforce strict singleton constraints across orchestrator and internal Ada server

--- Adelaide_Lite/src/version.ads
+++ Adelaide_Lite/src/version.ads
@@ -4,6 +4,6 @@
 --  ============================================================================
 package Version is
    Project_Name : constant String := "Project Zephyrine Adelaide Volatus Damare AWS";
-   Git_Hash     : constant String := "688579f";
+   Git_Hash     : constant String := "db6521b";
    Full_Version : constant String := Project_Name & " - " & Git_Hash;
 end Version;
