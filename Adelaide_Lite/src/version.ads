3b5b90c4ffe448513c8decfd7427001b2e6aa91f
Albert Starfield Wahyu Suryo Samudro <albertstarfield2001@gmail.com>
2026-06-29 16:39:50 +0700

    fix: tokenizer auto-resize for virtual ctx + prompt tokenization path

--- Adelaide_Lite/src/version.ads
+++ Adelaide_Lite/src/version.ads
@@ -4,6 +4,6 @@
 --  ============================================================================
 package Version is
    Project_Name : constant String := "Project Zephyrine Adelaide Volatus Damare AWS";
-   Git_Hash     : constant String := "2a28db7";
+   Git_Hash     : constant String := "98dcb20";
    Full_Version : constant String := Project_Name & " - " & Git_Hash;
 end Version;
