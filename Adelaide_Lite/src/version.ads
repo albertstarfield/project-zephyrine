0088a9268e0d4281ec91e1b0066389db93e8d6c3
Albert Starfield Wahyu Suryo Samudro <albertstarfield2001@gmail.com>
2026-07-03 12:24:02 +0700

    purge: remove SSL cert and key from git tracking
    adelaide-server.key purged from entire history via filter-branch.
    adelaide-server.crt removed from tracking (now gitignored by run/*).

--- Adelaide_Lite/src/version.ads
+++ Adelaide_Lite/src/version.ads
@@ -4,6 +4,6 @@
 --  ============================================================================
 package Version is
    Project_Name : constant String := "Project Zephyrine Adelaide Volatus Damare AWS";
-   Git_Hash     : constant String := "47ae51b";
+   Git_Hash     : constant String := "3f4c299";
    Full_Version : constant String := Project_Name & " - " & Git_Hash;
 end Version;
